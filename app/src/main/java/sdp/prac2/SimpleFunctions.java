e5f176734051b105b40e38423dc1f7b3cffbdc5e
Amogelang Mphela <G22A7399@ict.ru.ac.za>
2024-05-20 21:27:07 +0200

    The method for task4 completed

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,25 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    
+    public List<integer> task4(List<Integer> list1, List<Integer> list2){
+        int len1 = list1.size();
+        int len2 = list2.size();
+        List<Integer> myList = new ArrayList<>();
+
+        if (len1 != len2){
+            return null;
+        }
+        else{
+            for (int i = 0; i < len1; i++){
+                int num1 = list1.get(i);
+                int num2 = list2.get(len2-1);
+                len2--;
+                myList.add(num1 * num2);
+            }
+
+        }
+        return myList;
+    }
 }
