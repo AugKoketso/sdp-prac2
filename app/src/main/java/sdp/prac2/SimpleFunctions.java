c824987855f98c07a3f1e4f541cc8a15094e6d59
Makomborero23 <g22m3237@campus.ru.ac.za>
2024-05-20 20:33:53 +0200

    Added Task5 to SimpleFunctions.java

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -5,5 +5,18 @@ import java.util.*;
 public class SimpleFunctions {
     public SimpleFunctions() {}
 
-    
+    public static boolean Task5(int[] intList) {
+        if (intList == null || intList.length <= 1) {
+            return true; // A null or single element intList is considered sorted
+        }
+        
+        for (int i = 1; i < intList.length; i++) {
+            if (intList[i] < intList[i - 1]) {
+                return false; // Found an element out of order
+            }
+        }
+        
+        return true; // All elements are in order
+    }
+
 }
